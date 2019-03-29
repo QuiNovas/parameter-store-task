c7afd063891aa4895dc52581ce04c42036932803
Jishna Suresh <jsuresh@quinovas.com>
2019-03-29 18:30:36 +0530

    Made the required changes in input and output

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -1,9 +1,9 @@
 import boto3
-import json	
+import json
 import logging.config
 import os
 
-kms_key = os.environ['SSM_KMS_KEY_ARN']
+# KMS_KEY = os.environ['SSM_KMS_KEY_ARN']
 client = boto3.client('ssm')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -11,48 +11,64 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
 	logger.info('Event :{}'.format(event))
-	body = event['Body']
-	if 'Action' in body:
-		action = body['Action']
-		if action == 'GetParameter':
-			if 'Name' in body:
-				return get_parameter_from_store(body['Name'])
-			elif 'Names' in body:
-				return get_parameter_from_store(body['Names'])
-			else:
-				logger.info('Missing the parameter name in the event')
-		elif action == 'PutParameter':
-			return put_parameter_to_store(body['Name'], body['Type'], body['Value'])
-		else:
-			logger.info('Undefined action. Cannot complete the request')            					
+	if 'GetParameters' in event:
+		response = _get_parameter_from_store(event['GetParameters']['Names'])
+	elif 'GetParametersByPath' in event:
+		response = _get_parameter_from_store_by_path(event['GetParametersByPath']['Path'], event['GetParametersByPath']['Recursive'])
+	elif 'PutParameter' in event:
+		response = _put_parameter_to_store(event['PutParameter'])
 	else:
-		logger.info('Missing action (get/put) in event body')	
+		return 'Unexpected request. Cannot be completed.'
+	return response
+	
 
-		
-def get_parameter_from_store(name):
-	if isinstance(name, list):
-		parameter_list = []
-		for each_name in name:
-			params = {}
-			response = client.get_parameter(Name=each_name, WithDecryption=True)
-			params['Name'] = each_name
-			params['Value'] = response['Parameter']['Value']
-			parameter_list.append(params)
-		return parameter_list
-	else:
-		response = client.get_parameter(Name=name, WithDecryption=True)
-		return 'Value:{}'.format(response['Parameter']['Value'])
-		
+def _get_parameter_from_store(names):
+	parameters = {}
+	parameters_list = {}
+	for name in names:
+		params = client.get_parameter(Name=name, WithDecryption=True)
+		parameters[params['Parameter']['Name']] = params['Parameter']
+		parameters_list["Parameters"] = parameters
+	return json.dumps(parameters_list, indent=2, default=str)
+	
 
-		
-def put_parameter_to_store(name, type_of_parameter, value):
-	if type_of_parameter == "SecureString":
-		response = client.put_parameter(KeyId=kms_key, Name=name, Type=type_of_parameter, Value=value)
-		return response['Version']
-	elif type_of_parameter == "String":
-		response = client.put_parameter(Name=name, Type=type_of_parameter, Value=value)
-		return 'Version:{}'.format(response['Version'])
+def _get_parameter_from_store_by_path(path, recursive):
+	parameters = {}
+	response = {}
+	parameters_list = []
+	params = client.get_parameters_by_path(Path=path, Recursive=recursive)
+	for i in range(0, len(params['Parameters'])):
+		parameters[params['Parameters'][i]['Name']] = params['Parameters'][i]
+		parameters_list.append(parameters)
+	response["Parameters"] = parameters_list
+	return json.dumps(response, indent=2, default=str)
+	
+
+def _put_parameter_to_store(put_param):
+	
+		response = client.put_parameter(
+								Name=put_param['Name'],
+								Type=put_param['Type'],
+								Value=put_param['Value'],
+								KeyId=put_param['KeyId'],
+								Description=put_param.get('Description', ''),
+								Overwrite=put_param.get('Overwrite', False)
+								)
 	else:
-		logger.info('Unexpected type. Cannot put the parameter in store')
+		response = client.put_parameter(
+										Name=put_param['Name'],
+										Type=put_param['Type'],
+										Value=(",".join(put_param['Value']))  if put_param['Type']=='StringList' else put_param['Value'],
+										Description=put_param.get('Description', ''),
+										Overwrite=put_param.get('Overwrite', False)
+										)
+	return 'Version:{}'.format(response['Version'])
+								
+								
+								
+	
 		
-	#TODO for StringList
+		
+		
+		
+		
