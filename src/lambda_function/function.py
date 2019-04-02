d6d708fa67e43e91374ebdf522edee4219cb966d
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-04-02 11:11:10 -0500

    Final update

--- src/lambda_function/function.py
+++ src/lambda_function/function.py
@@ -35,15 +35,28 @@ def _get_parameters_by_path(path, recursive):
 	)
 	return _transform_return_parameters(response.get('Parameters', []))
 
-
 def _transform_return_parameters(parameters):
 	transformed_parameters = {}
 	for parameter in parameters:
-		transformed_parameters[parameter['Name']] = \
-			parameter['Value'] if \
-				parameter['Type'] != 'StringList' else \
-					parameter['Value'].split(',')
+		name = parameter['Name']
+		value = parameter['Value'] if parameter['Type'] != 'StringList' else parameter['Value'].split(',')
+		if name.startswith('/'):
+			name_elements = name[1:].split('/')
+			node = _make_or_traverse_path(transformed_parameters, name_elements[:-1])
+			if not isinstance(node, dict):
+				raise ValueError('Parameter {} is both a node and a leaf. This is not supported.'.format('/'.join(name_elements[:-1])))
+			node[name_elements[-1:][0]] = value
+		else:
+			transformed_parameters[name] = value
 	return transformed_parameters
+
+
+def _make_or_traverse_path(parameters, elements):
+	if not elements:
+		return parameters
+	if elements[0] not in parameters:
+		parameters[elements[0]] = {}
+	return _make_or_traverse_path(parameters[elements[0]], elements[1:])
 
 
 def _put_parameter(parameter):
